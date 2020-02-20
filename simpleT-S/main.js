a5a988ef615af654c5c34ada37e68cea79bcec42
antoNanahJi <Anto.NanahJi@georgebrown.ca>
2020-02-19 19:31:27 -0500

    Added TTS

---
+++ simpleT-S/main.js
@@ -0,0 +1,63 @@
+var synth;
+
+
+var voiceSelect ;
+
+
+
+var voices ;
+
+
+
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for(i = 0; i < voices.length ; i++) {
+    var option = document.createElement("option");
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+    
+    if(voices[i].default) {
+      option.textContent += " -- DEFAULT";
+    }
+
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
+
+
+
+function myFunction(inputTxt) {
+ 
+
+  var utterThis = new SpeechSynthesisUtterance();
+  var selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+  for(i = 0; i < voices.length ; i++) {
+    if(voices[i].name === selectedOption) {
+      utterThis.voice = voices[i];
+    }
+  }
+  utterThis.pitch = 2.0;
+  utterThis.rate = 1;
+  utterThis.text = inputTxt;
+  synth.speak(utterThis);
+
+}
+
+window.onload =function()
+{
+	synth  = window.speechSynthesis;
+	voiceSelect = document.getElementById("voiceSelect");
+	voices = [];
+	
+	populateVoiceList();
+	if (speechSynthesis.onvoiceschanged !== undefined) {
+	  speechSynthesis.onvoiceschanged = populateVoiceList;
+	
+	}
+  	
+}
+
+function myMFunction(){ myFunction("Usama is idiot"); }
+
