f8d4ca8c87b24570a210def9ccdb10f4dd03c611
hassan <hassanjkhan6@gmail.com>
2020-02-20 11:04:26 -0500

    speak when wrists unaligned

--- simpleT-S/main.js
+++ simpleT-S/main.js
@@ -1,43 +1,34 @@
 var synth;
-
-
 var voiceSelect ;
-
-
-
 var voices ;
-
-
 
 function populateVoiceList() {
   voices = synth.getVoices();
-
-  for(i = 0; i < voices.length ; i++) {
-    var option = document.createElement("option");
-    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+  
+  // for(i = 0; i < voices.length ; i++) {
+  //   var option = document.createElement("option");
+  //   option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
     
-    if(voices[i].default) {
-      option.textContent += " -- DEFAULT";
-    }
+  //   if(voices[i].default) {
+  //     option.textContent += " -- DEFAULT";
+  //   }
 
-    option.setAttribute("data-lang", voices[i].lang);
-    option.setAttribute("data-name", voices[i].name);
-    voiceSelect.appendChild(option);
-  }
+  //   option.setAttribute("data-lang", voices[i].lang);
+  //   option.setAttribute("data-name", voices[i].name);
+  //   voiceSelect.appendChild(option);
+  // }
 }
 
-
-
 function myFunction(inputTxt) {
- 
-
   var utterThis = new SpeechSynthesisUtterance();
-  var selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
-  for(i = 0; i < voices.length ; i++) {
-    if(voices[i].name === selectedOption) {
-      utterThis.voice = voices[i];
-    }
-  }
+  //var selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+  // for(i = 0; i < voices.length ; i++) {
+  //   if(voices[i].name === selectedOption) {
+  //     utterThis.voice = voices[i];
+  //   }
+  // }
+  console.log("say voice!");
+  utterThis.voice = voices[0];
   utterThis.pitch = 2.0;
   utterThis.rate = 1;
   utterThis.text = inputTxt;
@@ -56,7 +47,7 @@ window.onload =function()
 	  speechSynthesis.onvoiceschanged = populateVoiceList;
 	
 	}
-  	
+    this.myFunction("Usama is amazing");
 }
 
 function myMFunction(){ myFunction("Usama is idiot"); }
